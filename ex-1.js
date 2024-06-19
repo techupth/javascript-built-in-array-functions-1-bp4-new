c3a37a07933a865be7d865c0f061129e1b030123
Wasin <wasin1026@gmail.com>
2024-06-19 22:25:19 +0700

    Exercise #1

--- ex-1.js
+++ ex-1.js
@@ -2,9 +2,11 @@ let words = ["hello", "world"];
 
 function getWordLengths(words) {
   // Start coding here
+  return words.map(getLength)
+}
+function getLength(words){
   return words.length;
 }
+const result = getWordLengths(words);
 
-const result = words.map(getWordLengths);
-
-console.log(result); // Output: [5, 5]
+console.log(result);
