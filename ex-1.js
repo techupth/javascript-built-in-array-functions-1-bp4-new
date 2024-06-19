7d801c8fa58c67188a23e4739100c0a41ed9f0a7
Wasin <wasin1026@gmail.com>
2024-06-19 22:52:12 +0700

    EX1

--- ex-1.js
+++ ex-1.js
@@ -2,10 +2,9 @@ let words = ["hello", "world"];
 
 function getWordLengths(words) {
   // Start coding here
-  return words.map(getLength)
-}
-function getLength(words){
-  return words.length;
+  return words.map(function (words) {
+    return words.length;
+  });
 }
 const result = getWordLengths(words);
 
